f5c41584ec84492c3032dee09c1c28d6a55402bf
DSRKafuU <21136293+dsrkafuu@users.noreply.github.com>
2026-09-25 14:48:13 +0800

    chore: update build scripts to remove unused environment variables

--- .tools/cargo-webrtc.cmd
+++ .tools/cargo-webrtc.cmd
@@ -1,7 +1,5 @@
 @echo off
 call "C:\Program Files (x86)\Microsoft Visual Studio\18\BuildTools\Common7\Tools\VsDevCmd.bat" -arch=x64 -host_arch=x64
 if errorlevel 1 exit /b %errorlevel%
-set "PYTHONPATH=%~dp0python"
-set "LIBCLANG_PATH=%~dp0python\clang\native"
-set "PATH=%~dp0bin;%~dp0python\bin;%PATH%;C:\PortableSDKs\PortableGit\usr\bin"
+set "LIBCLANG_PATH=%VSINSTALLDIR%VC\Tools\Llvm\x64\bin"
 cargo %*
--- .tools/rust-analyzer.cmd
+++ .tools/rust-analyzer.cmd
@@ -4,10 +4,7 @@ setlocal
 call "C:\Program Files (x86)\Microsoft Visual Studio\18\BuildTools\Common7\Tools\VsDevCmd.bat" -arch=x64 -host_arch=x64 >nul
 if errorlevel 1 exit /b %errorlevel%
 
-set "PYTHONPATH=%~dp0python"
-set "LIBCLANG_PATH=%~dp0python\clang\native"
-set "PATH=%~dp0bin;%~dp0python\bin;%PATH%;C:\PortableSDKs\PortableGit\usr\bin"
-
+set "LIBCLANG_PATH=%VSINSTALLDIR%VC\Tools\Llvm\x64\bin"
 cargo %*
 set "rustAnalyzerExitCode=%errorlevel%"
 endlocal & exit /b %rustAnalyzerExitCode%
